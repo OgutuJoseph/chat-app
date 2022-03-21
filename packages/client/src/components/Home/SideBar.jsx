5b36dde659c67707f1e9e50efb053e83f1a107db
OgutuJoseph <onyango.joseph@yahoo.com>
2022-03-21 04:09:47 +0300

    React Modal / Form with ChakraUI

--- packages/client/src/components/Home/SideBar.jsx
+++ packages/client/src/components/Home/SideBar.jsx
@@ -1,41 +1,49 @@
 import { VStack, HStack, Heading, Divider, Text, Circle } from '@chakra-ui/layout';
+import { useDisclosure } from '@chakra-ui/hooks';
 import { Button } from '@chakra-ui/button';
 import { ChatIcon } from '@chakra-ui/icons';
 import { TabList, Tab } from '@chakra-ui/tabs';
 import { useContext } from 'react';
 import { FriendContext } from './Home';
 
+import AddFriendModal from './AddFriendModal';
+
 const SideBar = () => {
 
     const { friendList, setFriendList } = useContext(FriendContext);
 
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
-        <VStack py='1.4rem'>
-            <HStack justify='space-evenly' w='100%'>
-                <Heading size='md'>Add Friend</Heading>
-                <Button>
-                    <ChatIcon />
-                </Button>
-            </HStack>
-            <Divider />
-            <VStack as={TabList}>
-                {/* <HStack as={Tab}>
-                    <Circle bg='red.500' w='10px' h='10px' />
-                    <Text>John Doe</Text>
+        <>
+            <VStack py='1.4rem'>
+                <HStack justify='space-evenly' w='100%'>
+                    <Heading size='md'>Add Friend</Heading>
+                    <Button onClick={onOpen}>
+                        <ChatIcon />
+                    </Button>
                 </HStack>
-                <HStack as={Tab}>
-                    <Circle bg='green.500' w='10px' h='10px' />
-                    <Text>Bill Gates</Text>
-                </HStack> */}
-                {friendList.map(friend => (
-                    <HStack as={Tab}>
-                        <Circle bg={friend.connected ? 'green.500' : 'red.500'} w='10px' h='10px' />
-                        <Text>{friend.username}</Text>
+                <Divider />
+                <VStack as={TabList}>
+                    {/* <HStack as={Tab}>
+                        <Circle bg='red.500' w='10px' h='10px' />
+                        <Text>John Doe</Text>
                     </HStack>
-                ))}
+                    <HStack as={Tab}>
+                        <Circle bg='green.500' w='10px' h='10px' />
+                        <Text>Bill Gates</Text>
+                    </HStack> */}
+                    {friendList.map(friend => (
+                        <HStack as={Tab}>
+                            <Circle bg={friend.connected ? 'green.500' : 'red.500'} w='10px' h='10px' />
+                            <Text>{friend.username}</Text>
+                        </HStack>
+                    ))}
+                </VStack>
             </VStack>
-        </VStack>
-    )
-}
+            <AddFriendModal isOpen={isOpen} onClose={onClose} />
+        </>
+    );
+};
 
 export default SideBar;
